b5b8689b3850cfc09fdd0ac1e66b055ae652980a
Zachary Yedidia <zyedidia@gmail.com>
2020-09-23 00:35:43 -0400

    Minor optimization

--- vm/vm.go
+++ vm/vm.go
@@ -117,30 +117,30 @@ loop:
 			vm.st.pop()
 			goto fail
 		case opTestChar:
-			lbl := decodeU32(code[vm.ip+1:])
 			b := decodeByte(code[vm.ip+1+4:])
 			in, eof := vm.input.Peek()
 			if eof == nil && in == b {
 				vm.input.Advance(1)
 				vm.ip += 6
 			} else {
+				lbl := decodeU32(code[vm.ip+1:])
 				vm.ip = int(lbl)
 			}
 		case opTestSet:
-			lbl := decodeU32(code[vm.ip+1:])
 			set := decodeSet(code[vm.ip+1+4:])
 			in, eof := vm.input.Peek()
 			if eof == nil && set.Has(in) {
 				vm.input.Advance(1)
 				vm.ip += 21
 			} else {
+				lbl := decodeU32(code[vm.ip+1:])
 				vm.ip = int(lbl)
 			}
 		case opTestAny:
-			lbl := decodeU32(code[vm.ip+1:])
 			n := decodeByte(code[vm.ip+1+4:])
 			err := vm.input.Advance(int(n))
 			if err != nil {
+				lbl := decodeU32(code[vm.ip+1:])
 				vm.ip = int(lbl)
 			} else {
 				vm.ip += 6
